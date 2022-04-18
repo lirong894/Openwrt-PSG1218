30ae33975938955036c46929181b65f1172a8854
gooduc <gooduc@gmail.com>
2022-04-18 23:08:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,10 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.2/g' package/base-files/files/bin/config_generate
+
+#删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
 # 去除默认主题
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
